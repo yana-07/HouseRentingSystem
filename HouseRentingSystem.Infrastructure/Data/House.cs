324ca0739a4361793b77fabcd7e6855ac16ab144
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-03 16:40:34 +0200

    create entity models

---
+++ HouseRentingSystem.Infrastructure/Data/House.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HouseRentingSystem.Infrastructure.Data
+{
+    public class House
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(150)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [StringLength(200)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Column(TypeName = "money")]
+        [Precision(18, 2)]
+        public decimal PricePerMonth { get; set; }
+
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; } = null!;
+
+        [ForeignKey(nameof(Agent))]
+        public int AgentId { get; set; }
+
+        public Agent Agent { get; set; }
+
+        [ForeignKey(nameof(Renter))]
+        public string? RenterId { get; set; }
+
+        public IdentityUser? Renter { get; set; }
+    }
+}
