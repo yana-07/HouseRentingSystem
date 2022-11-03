894ed6abf238479d5dabd37e369e07741c76c5b7
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-03 16:44:00 +0200

    add and apply entity configurations for seeding data

---
+++ HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Configuration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<House> Houses { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<Agent> Agents { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new AgentConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new HouseConfiguration());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
