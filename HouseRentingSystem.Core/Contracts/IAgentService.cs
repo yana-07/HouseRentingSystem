ef8456749ed03efcd93d2bb9130d9a262581944e
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-14 15:13:35 +0200

    add agents service interface and implementation

---
+++ HouseRentingSystem.Core/Contracts/IAgentService.cs
@@ -0,0 +1,13 @@
+﻿namespace HouseRentingSystem.Core.Contracts
+{
+    public interface IAgentService
+    {
+        Task<bool> ExistsByIdAsync(string userId);
+
+        Task<bool> UserWithPhoneNumerExistsAsync(string phoneNumber);
+
+        Task<bool> UserHasRentsAsync(string userId);
+
+        Task CreateAsync(string userId, string phoneNumber);
+    }
+}
---
+++ HouseRentingSystem.Core/Services/AgentService.cs
@@ -0,0 +1,47 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Infrastructure.Data;
+using HouseRentingSystem.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Core.Services
+{
+    public class AgentService : IAgentService
+    {
+        private readonly IRepository repo;
+
+        public AgentService(IRepository _repo)
+        {
+            this.repo = _repo;
+        }
+
+        public async Task CreateAsync(string userId, string phoneNumber)
+        {
+            var agent = new Agent
+            {
+                UserId = userId,
+                PhoneNumber = phoneNumber,
+            };
+
+            await repo.AddAsync(agent);
+            await repo.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsByIdAsync(string userId)
+        {
+            return await repo.AllReadonly<Agent>()
+                .AnyAsync(a => a.UserId == userId);
+        }
+
+        public async Task<bool> UserHasRentsAsync(string userId)
+        {
+            return await repo.AllReadonly<House>()
+                .AnyAsync(h => h.RenterId == userId);             
+        }
+
+        public async Task<bool> UserWithPhoneNumerExistsAsync(string phoneNumber)
+        {
+            return await repo.AllReadonly<Agent>()
+                .AnyAsync(a => a.PhoneNumber == phoneNumber);
+        }
+    }
+}
