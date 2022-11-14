e1562edd34b2c54832e9d0cbc7d5f1ab097f7524
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-14 15:15:29 +0200

    add ClaimsPrincipal extension method to get currently logged in user's Id

---
+++ HouseRentingSystem/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace HouseRentingSystem.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+            => user.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
