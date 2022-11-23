1d940f9ee295c6a6cd5ab825a3384e74f583c290
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-23 15:29:06 +0200

    add IsRented property to House entity model

---
+++ HouseRentingSystem.Infrastructure/Migrations/20221123115227_HouseIsActiveAdded.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HouseRentingSystem.Infrastructure.Migrations
+{
+    public partial class HouseIsActiveAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Houses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsActive",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsActive",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Houses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsActive",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Houses");
+        }
+    }
+}
