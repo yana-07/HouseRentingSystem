97b0fb6fcd58ed5bc333b9dbc2dda89aed79c9b1
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-30 13:10:17 +0200

    add Administration area

---
+++ HouseRentingSystem/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Areas.Admin.Controllers
+{
+    public class AdminController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
