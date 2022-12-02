56151b999137b7021e0d8decbc1f1ab90650258f
yana.radeva7 <yana.radeva7@gmail.com>
2022-12-02 22:35:14 +0200

    implement display added and rented by the admin houses; display all users in the admin area

---
+++ HouseRentingSystem/Areas/Admin/Controllers/HouseController.cs
@@ -0,0 +1,34 @@
+﻿using HouseRentingSystem.Areas.Admin.Models;
+using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Areas.Admin.Controllers
+{
+    public class HouseController : BaseController
+    {
+        private readonly IHouseService houseService;
+        private readonly IAgentService agentService;
+
+        public HouseController(
+            IHouseService houseService,
+            IAgentService agentService)
+        {
+            this.houseService = houseService;
+            this.agentService = agentService;
+        }
+
+        public async Task<IActionResult> Mine()
+        {
+            var myHouses = new MyHousesViewModel();
+
+            var adminId = User.Id();
+            myHouses.RentedHouses = await houseService.AllHousesByUSerId(adminId);
+
+            var agentId = await agentService.GetAgentId(adminId);
+            myHouses.AddedHouses = await houseService.AllHousesByAgentId(agentId);
+
+            return View(myHouses);
+        }
+    }
+}
---
+++ HouseRentingSystem/Areas/Admin/Models/MyHousesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HouseRentingSystem.Core.Models.House;
+
+namespace HouseRentingSystem.Areas.Admin.Models
+{
+    public class MyHousesViewModel
+    {
+        public IEnumerable<HouseServiceModel> AddedHouses { get; set; } 
+            = Enumerable.Empty<HouseServiceModel>();
+
+        public IEnumerable<HouseServiceModel> RentedHouses { get; set; }
+            = Enumerable.Empty<HouseServiceModel>();
+    }
+}
